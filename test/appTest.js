0d1e61c60829deb27d88788e20b6724deb44a27b
Vamsi Ravi <ravivamsi@yahoo.com>
2018-06-28 15:14:03 -0500

    Working on the Schema Definition and Assertion

--- test/appTest.js
+++ test/appTest.js
@@ -1,29 +1,59 @@
-// const assert = require("chai").assert;
-//
-// const app = require("../app");
-//
-//
-// describe('App', function(){
-//   it('App Should return hello', function(){
-//     assert.equal(app(), 'hello');
-//   });
-// });
-
+var jsonSchemaDef = {
+  "type": "object",
+  "properties": {
+    "userId": {
+      "type": "integer",
+      "title": "The Userid Schema ",
+      "default": 0,
+      "examples": [
+        1
+      ]
+    },
+    "id": {
+      "type": "integer",
+      "title": "The Id Schema ",
+      "default": 0,
+      "examples": [
+        1
+      ]
+    },
+    "title": {
+      "type": "string",
+      "title": "The Title Schema ",
+      "default": "",
+      "examples": [
+        "sunt aut facere repellat provident occaecati excepturi optio reprehenderit"
+      ]
+    },
+    "body": {
+      "type": "string",
+      "title": "The Body Schema ",
+      "default": "",
+      "examples": [
+        "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+      ]
+    }
+  }
+};
 
 var should = require("should");
 var request = require("request");
 var expect = require("chai").expect;
 var mocha = require("mocha");
-
+var chai = require('chai');
+chai.use(require('chai-json-schema'));
+var sys = require('sys');
+var fs = require('fs');
 var base = "https://jsonplaceholder.typicode.com";
 var util = require("util");
-
+// var jsonSchemaDefinition = require('./../resources/jsonschema/samplejsonschem.json');
 
 describe('Simple App Test', function() {
   it('Returns the json response', function(done){
     request.get({ url: base + '/posts/1'},
     function(error, response, body) {
       expect(response.statusCode).to.equal(200);
+      expect(body).to.not.be.jsonSchema(jsonSchemaDef);
       console.log(body);
       done();
     });
